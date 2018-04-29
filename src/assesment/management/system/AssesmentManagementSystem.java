c4c00d8b0a11e7eda55f114dff4e32714a22c3f6
Kasun <kasunaratthanage@gmail.com>
2018-04-29 15:49:13 +0530

    first commit

---
+++ src/assesment/management/system/AssesmentManagementSystem.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assesment.management.system;
+
+/**
+ *
+ * @author Kasun
+ */
+public class AssesmentManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new FrontPage().setVisible(true);
+    }
+    
+}
